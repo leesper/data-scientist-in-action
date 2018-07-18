e1331553970e30053c4f2b5fce1129f846d20acc
leesper <pascal7718@gmail.com>
2018-07-18 16:07:24 +0800

    add scripts for build and pull images from docker hub

---
+++ docker_pull.sh
@@ -0,0 +1,6 @@
+docker build -t leesper/ubuntu-java
+docker build -t leesper/zookeeper 
+docker build -t leesper/hadoop-base 
+docker build -t leesper/hadoop-hive 
+docker build -t leesper/hadoop-spark 
+docker build -t leesper/hadoop-hbase 
